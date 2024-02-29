5c797e4974906037d754f4f12cbd15d005c8b354
JoselynCisneros1510 <joselyn@corporaciondfl.com>
2024-02-29 11:07:51 -0500

    actualizado 3

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def dividir(a, b):
     if b != 0:
         return a / b
     else:
-        return false
+        return "Error: No se puede dividir por cero"
 
 def main():
     num1 = float(input("Ingrese el primer número: "))
